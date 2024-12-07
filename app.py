d9687bc6dd7ba674a3a298032888c7df3d00181d
Shahid-khan015 <khan.shahidljpit2020@gmail.com>
2024-12-07 17:27:57 +0530

    Commit Added

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
+import os
+
+
+app = Flask(__name__)
+
+CORS(app)
+
+application = app
+
+out_dir = r'website/DataSet/Output/div'
+video_extensions = ('.mp4', '.avi', '.mov', '.mkv')
+video_links = {}
+mp4 = [mp4 for mp4 in os.listdir(out_dir) if mp4.endswith(video_extensions)]
+
+for links in mp4:
+    video_links[links.replace('.mp4' , ' ')] = os.path.join(out_dir, links)
+
+
+
+@app.route('/')
+def home():
+    return jsonify(video_links)
+
+
+
+@app.route('/<path:file_path>')
+def render(file_path):
+    try:
+        def generate(video_path):
+            with open(video_path, 'rb') as f:
+                while True:
+                    chunk = f.read(1024 * 1024)
+                    if not chunk:
+                        break
+                    yield chunk
+
+        return Response(generate(file_path), mimetype='video/mp4')
+    except Exception as e:
+        return f"An error occured : {e}"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
